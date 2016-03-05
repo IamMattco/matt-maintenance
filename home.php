4e7eb630fbe866986e832e94c601498217779374
dev.mroz@gmail.com <dev.mroz@gmail.com>
2016-03-05 12:00:28 +0100

    contact modal

--- home.php
+++ home.php
@@ -1,9 +1,18 @@
-<?php 
+<?php get_header(); ?>
 
-  get_header();
+<section class="page">
+  <div class="home-intro">
+    <h1><?php echo $mattwp['title'] ?></h1>
+    <h2><?php echo $mattwp['message'] ?></h2>
+  </div>
+</section>
 
-  echo 'Home.php';
+<div class="fadeinModal hide">
+  <a href="#" class="closeModal"><i class="fa fa-remove"></i></a>
+  <div class="home-intro">
+    <h2>Kontakt</h2>
+    <div class="description"><?php echo $mattwp['contact'] ?></div>
+  </div>
+</div>
 
-  get_footer();
-
-?>
+<?php get_footer(); ?>
